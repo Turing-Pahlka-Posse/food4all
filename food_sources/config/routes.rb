56959e9a5366fb34ee63d3668823d35f30c0db87
jk1dd <portajonk@gmail.com>
2017-05-12 14:52:11 -0600

    add test and functionality for store type show endpoint and update readme

--- food_sources/config/routes.rb
+++ food_sources/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :stores do
+        get '/find', to: 'find_stores#index'
+      end
       resources :stores
       resources :store_types
     end
