9d214bd150b583ef955202ac4dea524b18824369
Laura Turk <laura.lk.turk@gmail.com>
2017-05-12 14:15:09 -0600

    stubs

---
+++ food4all/src/stubs/store_types-stub.js
@@ -0,0 +1,92 @@
+[
+{
+id: 1,
+name: "Dollar Store",
+created_at: "2017-05-12T16:20:25.689Z",
+updated_at: "2017-05-12T16:20:25.689Z"
+},
+{
+id: 2,
+name: "Small Grocery Store",
+created_at: "2017-05-12T16:20:25.754Z",
+updated_at: "2017-05-12T16:20:25.754Z"
+},
+{
+id: 3,
+name: "Convenience Store",
+created_at: "2017-05-12T16:20:25.761Z",
+updated_at: "2017-05-12T16:20:25.761Z"
+},
+{
+id: 4,
+name: "Convenience Store With Gas",
+created_at: "2017-05-12T16:20:25.777Z",
+updated_at: "2017-05-12T16:20:25.777Z"
+},
+{
+id: 5,
+name: "Specialty Food Store",
+created_at: "2017-05-12T16:20:25.789Z",
+updated_at: "2017-05-12T16:20:25.789Z"
+},
+{
+id: 6,
+name: "Closed Specialty Food Store",
+created_at: "2017-05-12T16:20:25.795Z",
+updated_at: "2017-05-12T16:20:25.795Z"
+},
+{
+id: 7,
+name: "Supermarket",
+created_at: "2017-05-12T16:20:25.844Z",
+updated_at: "2017-05-12T16:20:25.844Z"
+},
+{
+id: 8,
+name: "Pharmacy",
+created_at: "2017-05-12T16:20:25.855Z",
+updated_at: "2017-05-12T16:20:25.855Z"
+},
+{
+id: 9,
+name: "Closed Small Grocery Store",
+created_at: "2017-05-12T16:20:25.916Z",
+updated_at: "2017-05-12T16:20:25.916Z"
+},
+{
+id: 10,
+name: "Superette",
+created_at: "2017-05-12T16:20:26.019Z",
+updated_at: "2017-05-12T16:20:26.019Z"
+},
+{
+id: 11,
+name: "Closed Convenience Store",
+created_at: "2017-05-12T16:20:26.056Z",
+updated_at: "2017-05-12T16:20:26.056Z"
+},
+{
+id: 12,
+name: "Discount Merchandise Market",
+created_at: "2017-05-12T16:20:26.247Z",
+updated_at: "2017-05-12T16:20:26.247Z"
+},
+{
+id: 13,
+name: "Supercenter",
+created_at: "2017-05-12T16:20:26.405Z",
+updated_at: "2017-05-12T16:20:26.405Z"
+},
+{
+id: 14,
+name: "Warehouse Club Store",
+created_at: "2017-05-12T16:20:26.419Z",
+updated_at: "2017-05-12T16:20:26.419Z"
+},
+{
+id: 15,
+name: "Closed Supermarket",
+created_at: "2017-05-12T16:20:26.606Z",
+updated_at: "2017-05-12T16:20:26.606Z"
+}
+]
